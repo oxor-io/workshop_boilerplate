0994f177742d7c6509e7124128b9b71c948cd200
Vladimir Kumalagov <vkumalagov@icloud.com>
2023-10-06 14:24:52 +0300

    feat: add template & upd sh script

---
+++ circuits/circuit.circom
@@ -0,0 +1,5 @@
+pragma circom 2.1.2;
+
+template Template() {}
+
+component main = Template();
--- run.sh
+++ run.sh
@@ -3,76 +3,96 @@
 # Stop execution if any step returns non-zero (non success) status
 set -e
 
-if [ ! $1 ]; then
-    echo "You should pass <name> of the existing <name>.circom template to run. Example: ./run.sh multiplier2"
-    exit 1
-fi
-CIRCUIT_NAME=$1
+CIRCUIT_NAME=
+INPUT_NAME=
+CEREMONY=
 
-if [ ! $2 ]; then
-    echo "Please pass the input file as second arg"
-    exit 1
-fi
-INPUT_NAME=$2
+while getopts "c:i:r:" opt; do
+  case $opt in
+    c)
+      CIRCUIT_NAME=$OPTARG
+      ;;
+    i)
+      INPUT_NAME=$OPTARG
+      ;;
+    r)
+      CEREMONY=$OPTARG
+      ;;
+    \?)
+      exit 1
+      ;;
+  esac
+done
 
 BUILD_DIR=build
+JS_FOLDER=${BUILD_DIR}/${CIRCUIT_NAME}_js
+WITNESS=witness.wtns
+
+POTS_DIR=pots # directory to keep PowersOfTau
+POWERTAU=14 # power value for "powersOfTau"
+
+PTAU_FILE=pot${POWERTAU}_0000.ptau
+PTAU_PATH=${POTS_DIR}/${PTAU_FILE}
+
+PROOF=${BUILD_DIR}/proof.json
+PUBLIC=${BUILD_DIR}/public.json
+
+CONTRIBUTED_PTAU_FILE=contributed_${PTAU_FILE}
+FINAL_PTAU=pot${POWERTAU}_final.ptau
+
+# Is circuit exist?
+if [ ! -f circuits/${CIRCUIT_NAME}.circom ]; then
+  echo "circuits/${CIRCUIT_NAME}.circom doesn't exist, exit..."
+  exit 3
+fi
+
+if [ ! -f ${INPUT_NAME}.json ]; then
+  echo "Input file: ${INPUT_NAME}.json is not exist"
+  exit 4
+fi
+
 if [ -d "$BUILD_DIR" ]; then
   echo "$BUILD_DIR exists, deliting..."
   rm -rf ./$BUILD_DIR
 fi
 
-echo "Creating new \"$BUILD_DIR\" dir"
-mkdir "$BUILD_DIR"
-
-if [ ! -f circuits/${CIRCUIT_NAME}.circom ]; then
-    echo "circuits/${CIRCUIT_NAME}.circom template doesn't exist, exit..."
-    exit 2
-fi
+echo "Creating new $BUILD_DIR dir"
+mkdir -p "$BUILD_DIR"
 
 echo "Building R1CS for circuit ${CIRCUIT_NAME}.circom"
-if ! circom circuits/${CIRCUIT_NAME}.circom --r1cs --wasm --sym --output "$BUILD_DIR"; then
-    echo "circuits/${CIRCUIT_NAME}.circom compilation to r1cs failed. Exiting..."
-    exit 3
+if ! circom circuits/${CIRCUIT_NAME}.circom --r1cs --wasm --sym -o $BUILD_DIR; then
+  echo "circuits/${CIRCUIT_NAME}.circom compilation to r1cs failed. Exiting..."
+  exit 5
 fi
 
 # echo "Info about circuits/${CIRCUIT_NAME}.circom R1CS constraints system"
 # snarkjs info -c ${BUILD_DIR}/${CIRCUIT_NAME}.r1cs
 
 echo "Generate witness"
-JS_FOLDER=${BUILD_DIR}/${CIRCUIT_NAME}_js
-WITNESS=witness.wtns
-node ${JS_FOLDER}/generate_witness.js ${JS_FOLDER}/${CIRCUIT_NAME}.wasm ${INPUT_NAME} ${WITNESS}
+node ${JS_FOLDER}/generate_witness.js ${JS_FOLDER}/${CIRCUIT_NAME}.wasm ${INPUT_NAME}.json ${WITNESS}
 
-echo "Move witness to build"
+# Move witness to build
 mv ${WITNESS} ${BUILD_DIR}
 
-# directory to keep PowersOfTau, zkeys, and other non-circuit-dependent files
-POTS_DIR=pots
-# power value for "powersOfTau" pre-generated setup files
-POWERTAU=10
-
-PTAU_FILE=pot${POWERTAU}_0000.ptau
-PTAU_PATH=${POTS_DIR}/${PTAU_FILE}
-
-if [ -d "$POTS_DIR" ]; then
-  echo "$POTS_DIR exists, no need to generate new..."
-  else
-    mkdir ${POTS_DIR}
-    snarkjs powersoftau new bn128 ${POWERTAU} ${PTAU_PATH} -v
+if [ -d $POTS_DIR ] && [ -f $PTAU_PATH ]; then
+  echo "No need to generate new POTS"
+else
+  echo "Generate new PTAU"
+  mkdir -p ${POTS_DIR}
+  snarkjs powersoftau new bn128 ${POWERTAU} ${PTAU_PATH}
 fi
 
-CONTRIBUTED_PTAU_FILE=contributed_${PTAU_FILE}
-snarkjs powersoftau contribute ${PTAU_PATH} ${POTS_DIR}/${CONTRIBUTED_PTAU_FILE} --name="First contribution" -v
-
-FINAL_PTAU=pot${POWERTAU}_final.ptau
-snarkjs powersoftau prepare phase2 ${POTS_DIR}/${CONTRIBUTED_PTAU_FILE} ${POTS_DIR}/${FINAL_PTAU} -v
+if [ -z $CEREMONY ]; then
+  echo "Ceremony required, start..."
+  snarkjs powersoftau contribute ${PTAU_PATH} ${POTS_DIR}/${CONTRIBUTED_PTAU_FILE} --name="First contribution"
+  snarkjs powersoftau prepare phase2 ${POTS_DIR}/${CONTRIBUTED_PTAU_FILE} ${POTS_DIR}/${FINAL_PTAU}
+fi
 
 snarkjs groth16 setup ${BUILD_DIR}/${CIRCUIT_NAME}.r1cs ${POTS_DIR}/${FINAL_PTAU} ${BUILD_DIR}/${CIRCUIT_NAME}_init.zkey
-snarkjs zkey contribute ${BUILD_DIR}/${CIRCUIT_NAME}_init.zkey ${BUILD_DIR}/${CIRCUIT_NAME}.zkey --name="1st Contributor Name" -v
+snarkjs zkey contribute ${BUILD_DIR}/${CIRCUIT_NAME}_init.zkey ${BUILD_DIR}/${CIRCUIT_NAME}.zkey --name="1st Contributor Name"
 snarkjs zkey export verificationkey ${BUILD_DIR}/${CIRCUIT_NAME}.zkey ${BUILD_DIR}/verification_key.json
 
-PROOF=${BUILD_DIR}/proof.json
-PUBLIC=${BUILD_DIR}/public.json
+echo "Start proving"
 snarkjs groth16 prove ${BUILD_DIR}/${CIRCUIT_NAME}.zkey ${BUILD_DIR}/${WITNESS} ${PROOF} ${PUBLIC}
 
 echo "==============RESULT=============="
